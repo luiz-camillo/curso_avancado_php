5e012620545391ff80bbb0db3d04c22d87bc1fb6
camillo <lazarettieb@gmail.com>
2026-08-05 19:10:45 -0300

    finalizando estruturas repetição

---
+++ 10_inclusao_codigo/1_include/index.php
@@ -0,0 +1,13 @@
+
+
+<?php
+
+  include"teste.php";
+
+  echo $c;
+
+?>
+
+<p>Após o Include</p>
+<p>Imprimindo codigo em php junto com html <?php echo $c + 2 ?><p>
+
---
+++ 10_inclusao_codigo/1_include/teste.php
@@ -0,0 +1,8 @@
+
+
+
+<?php
+  $c=5;
+?>
+
+<h1>Testando o Include<h1>
