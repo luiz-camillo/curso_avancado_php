4da88a90ab3b6f446a3996ad5b1c32ba3933c494
camillo <lazarettieb@gmail.com>
2026-08-24 23:19:22 -0300

    terminar

--- 14_orientacao_objetos/13_exercicio_noc_chamados/index.php
+++ 14_orientacao_objetos/13_exercicio_noc_chamados/index.php
@@ -12,3 +12,56 @@ $gerenciador = new GerenciadorChamados();
 $gerenciador->adicionar(new ChamadoCritico(1, "Link caiu no site X"));
 $gerenciador->adicionar(new ChamadoRotina(2, "Solicitação de troca de senha"));
 $gerenciador->listar(); */
+
+
+abstract class Chamado{
+  protected int $id;
+  protected string $descricao;
+  protected string $status;
+
+  function __construct(int $id, string $descricao){
+    $this->id = $id;
+    $this->descricao = $descricao;
+    $this->status = "Aberto";
+  }
+
+  function getId(): int {
+    return $this->id;
+  }
+
+  function getDescricao(): string {
+    return $this->descricao;
+  }
+
+  abstract function prioridade(): string; // função abstrata termina com ';' e não com '{}'
+  
+  }
+
+
+class ChamadoCritico extends Chamado{
+  function prioridade(): string {
+    return "Alta";   
+  }
+}
+
+class ChamadoRotina extends Chamado{
+  function prioridade(): string {
+    return "Baixa";
+  }
+}
+
+class GerenciadorChamados{
+  private array $chamado = [];
+
+  function adicionar(Chamado $chamado): void {
+    array_push($this->chamado, $chamado);
+  }
+
+  function listar(){
+    foreach($this->chamado as $chamado){
+      echo $chamado->getId() . " - " . $chamado->getDescricao() . " - " . $chamado->prioridade() . "<br>";
+    }
+    
+  }
+
+}
